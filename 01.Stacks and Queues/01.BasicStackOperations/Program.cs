1b715def9bfa3162ab05542d89ae767d9776dbd6
PeterTodorovv <petertodorovv@gmail.com>
2021-11-21 19:13:44 +0200

    21/11

---
+++ 01.Stacks and Queues/01.BasicStackOperations/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.BasicStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int nToPush = input[0];
+            int sToPop = input[1];
+            int x = input[2];
+
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 0; i < nToPush; i++)
+            {
+                int number = numbers[i];
+                stack.Push(number);
+            }
+
+            for(int i = 1; i <= sToPop; i++)
+            {
+                stack.Pop();
+            }
+
+
+            if (stack.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else if(stack.Count > 0)
+            {
+
+                int smallest = int.MaxValue;
+                while (stack.Count > 0)
+                {
+                    int n = stack.Pop();
+                    if (n < smallest) smallest = n;
+                }
+                Console.Write(smallest);
+                
+            }
+            else
+            {
+                Console.Write(0);
+            }
+        }
+    }
+}
---
+++ 01.Stacks and Queues/02.BasicQueueOperations/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.BasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int nToEnque = input[0];
+            int sToDeque = input[1];
+            int x = input[2];
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for(int i = 0; i < nToEnque; i++)
+            {
+                queue.Enqueue(numbers[i]);
+            }
+
+            for (int i = 0; i < sToDeque; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else if (queue.Count > 0)
+            {
+
+                int smallest = int.MaxValue;
+                while (queue.Count > 0)
+                {
+                    int n = queue.Dequeue();
+                    if (n < smallest) smallest = n;
+                }
+                Console.Write(smallest);
+
+            }
+            else
+            {
+                Console.Write(0);
+            }
+        }
+    }
+}
