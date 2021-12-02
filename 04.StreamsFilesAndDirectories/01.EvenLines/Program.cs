d7db52803496bacfdaee6ecaf6d66fe266fafa50
PeterTodorovv <petertodorovv@gmail.com>
2021-12-02 23:15:34 +0200

    12/2

---
+++ 04.StreamsFilesAndDirectories/01.EvenLines/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace _01.EvenLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(StreamReader reader = new StreamReader("../../../input.txt"))
+            {
+                using(StreamWriter writer = new StreamWriter("../../../output.txt"))
+                {
+                    string line = reader.ReadLine();
+                    int index = 0;
+
+                    while (line != null)
+                    { 
+                        if (index % 2 == 0)
+                        {
+                            foreach(char chr in line)
+                            {
+                                if(chr == '-' || chr == ',' || chr == '.' || chr == '!' || chr == '?')
+                                {
+                                    line = line.Replace(chr, '@');
+                                }
+                            }
+
+                            string[] words = line.Split();
+                            for(int i = words.Length - 1; i >= 0; i--)
+                            {
+                                writer.Write(words[i] + " ");
+                            }
+                            writer.WriteLine();
+                        }
+
+                        index++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
