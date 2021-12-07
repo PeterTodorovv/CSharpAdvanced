d5d028a9cb0ce4fb6aad9504e3ae45949bd45b9f
PeterTodorovv <petertodorovv@gmail.com>
2021-12-07 12:20:36 +0200

    12/7

---
+++ 04.StreamsFilesAndDirectories/02.LineNumbers2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace _02.LineNumbers2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("../../../text.txt");
+
+
+        }
+    }
+}
