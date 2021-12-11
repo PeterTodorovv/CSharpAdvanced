709213badf8c6faeca271b32fd95050bd45aa570
PeterTodorovv <petertodorovv@gmail.com>
2021-12-11 19:20:29 +0200

    12/11

--- 04.StreamsFilesAndDirectories/02.LineNumbers2/Program.cs
+++ 04.StreamsFilesAndDirectories/02.LineNumbers2/Program.cs
@@ -7,10 +7,46 @@ namespace _02.LineNumbers2
     {
         static void Main(string[] args)
         {
-            var lines = File.ReadAllLines("../../../text.txt");
+            var lines = File.ReadAllLines("../../../input.txt");
+            string[] newLines = new string[lines.Length];
+            int count = 1;
 
-            File.WriteAllLines("../../../", lines);
+            foreach(var line in lines)
+            {
+                string newLine = $"Line {count}: {line} ({CountLetters(line)})({CountPunctuatuin(line)})";
+                newLines[count-1] = newLine;
+                count++;
+            }
+            File.WriteAllLines("../../../output.txt", newLines);
 
         }
+
+        public static int CountLetters(string line)
+        {
+            int letters = 0;
+
+            foreach(var chr in line)
+            {
+                if (char.IsLetter(chr))
+                    letters++;
+            }
+
+            return letters;
+        }
+
+        public static int CountPunctuatuin(string line)
+        {
+            int ponctuation = 0;
+
+            foreach(var chr in line)
+            {
+                if (char.IsPunctuation(chr))
+                {
+                    ponctuation++;
+                }
+            }
+
+            return ponctuation;
+        } 
     }
 }
