8f7c477742f48c8c9abcd644c172f88227712c75
PeterTodorovv <petertodorovv@gmail.com>
2021-12-15 22:37:00 +0200

    12/15

---
+++ 05.FunctionalProgramming/07.PredicateForNames/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+
+            foreach(string name in names.Where(name => name.Length <= n))
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ 05.FunctionalProgramming/08.ListOfPredicates/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] dividers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Func<int, int[], bool> isDividable = IsDividable;
+            Func<int, int[], Func<int, int[], bool>, List<int>> filter = Filter;
+
+            List<int> numbers = filter(n, dividers, isDividable);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        static bool IsDividable(int num, int[] dividers)
+        {
+            foreach (var divider in dividers)
+            {
+                if (num % divider != 0)
+                    return false;
+            }
+            return true;
+        }
+
+        static List<int> Filter(int n, int[] dividers, Func<int, int[], bool> isDividable)
+        {
+            List<int> numbers = new List<int>();
+
+            for(int i = 1; i <= n; i++)
+            {
+                if (isDividable(i, dividers))
+                    numbers.Add(i);
+            }
+
+            return numbers;
+        }
+    }
+}
