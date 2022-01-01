508d5e819bd498d7e37301d76e3a3d1c7ae3dde6
PeterTodorovv <petertodorovv@gmail.com>
2022-01-01 22:46:44 +0200

    01/01

---
+++ 07.CustomDoublyLinkedList/07.CustomDoublyLinkedList/StartUp .cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _07.CustomDoublyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
