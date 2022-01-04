828ca791050c56db099dc349b733e711151bc788
PeterTodorovv <petertodorovv@gmail.com>
2022-01-04 20:21:15 +0200

    01/04

--- 08.ImplementingStackAndQueue/CustomQueue/CustomQueue.cs
+++ 08.ImplementingStackAndQueue/CustomQueue/CustomQueue.cs
@@ -10,12 +10,87 @@ namespace CustomQueue
         private const int defaultSize = 4;
         public int Count { get; private set; }
 
+        private const int firstItemIndex = 0;
+
         public CustomQueue()
         {
             this.items = new T[defaultSize];
             this.Count = 0;
         }
 
+        public void Enque(T element)
+        {
+            if(Count == items.Length)
+            {
+                Resize();
+            }
 
+            items[Count] = element;
+            Count++;
+            
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            for(int i = 0; i < Count; i++)
+            {
+                action(items[i]);
+            }
+        }
+
+        public T Deque()
+        {
+            IsEmpty();
+            var removed = items[firstItemIndex];
+            items[firstItemIndex] = default;
+            ShiftLeft();
+            Count--;
+            return removed;
+        }
+
+       
+
+        public T Peek()
+        {
+            IsEmpty();
+            return items[firstItemIndex];
+        }
+
+        public void Clear()
+        {
+            IsEmpty();
+            Count = 0;
+            items = new T[defaultSize];
+        }
+
+
+
+        private void ShiftLeft()
+        {
+            for(int i = 1; i < Count; i++)
+            {
+                items[i - 1] = items[i];
+            }
+        }
+
+        private void IsEmpty()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        private void Resize()
+        {
+            T[] newArray = new T[items.Length * 2];
+
+            for(int i = 0; i < Count; i++)
+            {
+                newArray[i] = items[i];
+            }
+
+            items = newArray;
+        }
     }
 }
--- 08.ImplementingStackAndQueue/CustomQueue/Program.cs
+++ 08.ImplementingStackAndQueue/CustomQueue/Program.cs
@@ -6,7 +6,20 @@ namespace CustomQueue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CustomQueue<int> queue = new CustomQueue<int>();
+
+            queue.Enque(1);
+            queue.Enque(2);
+            queue.Enque(3);
+            queue.Enque(4);
+            queue.Enque(5);
+            Console.WriteLine(queue.Deque());
+            Console.WriteLine(queue.Peek());
+            queue.ForEach(Console.WriteLine);
+            queue.Clear();
+            queue.Deque();
+            
+            
         }
     }
 }
