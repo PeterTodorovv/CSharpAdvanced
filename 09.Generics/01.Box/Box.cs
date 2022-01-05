a1c5b1aff8434d80a4fa29a0bb0db5dc84fcf0b5
PeterTodorovv <petertodorovv@gmail.com>
2022-01-05 23:15:35 +0200

    01/05

---
+++ 09.Generics/01.Box/Box.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public List<T> list;
+        public int Count { get { return this.list.Count; } }
+
+
+        public Box()
+        {
+            this.list = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            this.list.Insert(0, element);
+        }
+
+        public T Remove()
+        {
+            var element = this.list[0];
+            list.RemoveAt(0);
+            return element;
+        }
+    }
+}
