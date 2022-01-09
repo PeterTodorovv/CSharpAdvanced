39a552df77f0c8250721845a696d48cfd22949c4
PeterTodorovv <petertodorovv@gmail.com>
2022-01-09 20:50:18 +0200

    01/09

---
+++ 10.IteratorsAndComparators/IteratorsAndComparators/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        IReadOnlyList<string> Authors { get; set; }
+
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = authors;
+        }
+    }
+}
