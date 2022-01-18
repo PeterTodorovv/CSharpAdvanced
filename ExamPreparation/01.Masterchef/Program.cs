a7f72027556518a767b5e31e94dfe7d4e7941927
PeterTodorovv <petertodorovv@gmail.com>
2022-01-18 21:25:28 +0200

    01/18

--- ExamPreparation/01.Masterchef/Program.cs
+++ ExamPreparation/01.Masterchef/Program.cs
@@ -9,7 +9,7 @@ namespace _01.Masterchef
         static void Main(string[] args)
         {
             Queue<int> numberOfIngridiants = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
-            Stack<int> freshness = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            Stack<int> freshness = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
             Dictionary<string, int> dishes = new Dictionary<string, int>()
             {
                 { "Dipping sauce", 0 },
@@ -20,35 +20,43 @@ namespace _01.Masterchef
             
             while(numberOfIngridiants.Count > 0 && freshness.Count > 0)
             {
-                int ingridinetValue = numberOfIngridiants.Dequeue();
-                int ingridientFreshness = freshness.Pop();
-
-                if (ingridinetValue == 0)
-                    ingridinetValue = numberOfIngridiants.Dequeue();
-
-                if (ingridientFreshness == 0)
-                    ingridientFreshness = freshness.Pop();
+                int ingridinetValue = numberOfIngridiants.Peek();
+                int ingridientFreshness = freshness.Peek();
 
                 int dishValue = ingridinetValue * ingridientFreshness;
 
                 if(dishValue == 150)
                 {
                     dishes["Dipping sauce"]++;
+                    numberOfIngridiants.Dequeue();
+                    freshness.Pop();
                 }
                 else if(dishValue == 250)
                 {
                     dishes["Green salad"]++;
+                    numberOfIngridiants.Dequeue();
+                    freshness.Pop();
                 }
                 else if (dishValue == 300)
                 {
                     dishes["Chocolate cake"]++;
+                    numberOfIngridiants.Dequeue();
+                    freshness.Pop();
                 }
                 else if (dishValue == 400)
                 {
                     dishes["Lobster"]++;
+                    numberOfIngridiants.Dequeue();
+                    freshness.Pop();
+                }
+                else if(ingridinetValue == 0)
+                {
+                    numberOfIngridiants.Dequeue();
                 }
                 else
                 {
+                    numberOfIngridiants.Dequeue();
+                    freshness.Pop();
                     numberOfIngridiants.Enqueue(ingridinetValue + 5);
                 }
             }
@@ -58,7 +66,7 @@ namespace _01.Masterchef
             else
                 Console.WriteLine("Applause! The judges are fascinated by your dishes!");
 
-            if (numberOfIngridiants.Count != 0)
+            if (numberOfIngridiants.Count > 0)
                 Console.WriteLine($"Ingredients left: {numberOfIngridiants.Sum()}");
 
             foreach(var dish in dishes.Where(d => d.Value != 0).OrderBy(d => d.Key))
---
+++ ExamPreparation/02.Survivorr/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Survivorr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int myTokens = 0;
+            int oponentsTokens = 0;
+            char[][] beach = ReadBeach(n);
+            string[] input = Console.ReadLine().Split();
+            
+            while(input[0] != "Gong")
+            {
+                string command = input[0];
+                int row = int.Parse(input[1]);
+                int col = int.Parse(input[2]);
+
+                if(command == "Find" && isValid(row, col, beach) && beach[row][col] == 'T')
+                {
+                    myTokens++;
+                    beach[row][col] = '-';
+                }
+                else if(command == "Opponent" && isValid(row, col, beach) && beach[row][col] == 'T')
+                {
+                    oponentsTokens++;
+                    beach[row][col] = '-';
+                    string direction = input[3];
+
+                    if (direction == "right")
+                    {
+                        for (int i = col + 1; i <= col + 3; i++)
+                        {
+                            if (isValid(row, i, beach))
+                            {
+                                if (beach[row][i] == 'T')
+                                {
+                                    oponentsTokens++;
+                                    beach[row][i] = '-';
+                                }
+                            }
+                        }
+                    }
+                    else if (direction == "left")
+                    {
+                        for (int i = col - 1; i >= col - 3; i--)
+                        {
+                            if (isValid(row, i, beach))
+                            {
+                                if (beach[row][i] == 'T')
+                                {
+                                    oponentsTokens++;
+                                    beach[row][i] = '-';
+                                }
+                            }
+                        }
+                    }
+                    else if (direction == "up")
+                    {
+                        for (int i = row - 1; i >= row - 3; i--)
+                        {
+                            if(isValid(i, col, beach))
+                            {
+                                if(beach[i][col] == 'T')
+                                {
+                                    oponentsTokens++;
+                                    beach[i][col] = '-';
+                                }
+                            }
+                        }
+                    }
+                    else if(direction == "down")
+                    {
+                        for (int i = row + 1; i <= row + 3; i++)
+                        {
+                            if (isValid(i, col, beach))
+                            {
+                                if (beach[i][col] == 'T')
+                                {
+                                    oponentsTokens++;
+                                    beach[i][col] = '-';
+                                }
+                            }
+                        }
+                    }
+
+                }
+
+
+                input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            PrintBeach(beach);
+            Console.WriteLine($"Collected tokens: {myTokens}");
+            Console.WriteLine($"Opponent's tokens: {oponentsTokens}");
+        }
+
+        private static bool isValid(int row, int col, char[][] beach)
+        {
+            if(row >= 0 && row < beach.Length && col >= 0 && col < beach[row].Length)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static char[][] ReadBeach(int n)
+        {
+            char[][] beach = new char[n][];
+
+            for(int i = 0; i < n; i++)
+            {
+                char[] row = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                beach[i] = row;
+            }
+
+            return beach;
+        }
+
+        private static void PrintBeach(char[][] beach)
+        {
+            for (int i = 0; i < beach.Length; i++)
+            {
+                Console.WriteLine(string.Join(" ", beach[i]));
+            }
+
+        }
+    }
+}
